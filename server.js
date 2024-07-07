43673447fa3f656288b8ca1e93efb0f099023373
vyshnavi-avasarala <vyshnaviavasarala.2019@gmail.com>
2024-07-07 20:37:31 +0530

    commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+//app.use(express.json());
+
+app.get('/',
+	(req, res) => {
+		res.sendFile(__dirname + '/index.html');
+	});
+
+app.get('/login',
+	(req, res) => {
+		res.sendFile(__dirname + '/login.html');
+	});
+app.get('/register',
+	(req, res) => {
+		res.sendFile(__dirname + '/register.html');
+	});
+app.get('/welcome',
+	(req, res) => {
+		res.sendFile(__dirname + '/welcome.html');
+	});
+app.post('/',(req, res) => {
+		res.redirect("/")
+		});
+app.post('/login',(req, res) => {
+	res.redirect("/login")
+	});
+app.post('/register',(req, res) => {
+        res.redirect("/register")
+        });
+app.post('/welcome',(req, res) => {
+	res.redirect("/welcome")
+	});
+
+app.listen(3000,
+	() => {
+		console.log(
+			'Our express server is up on port 3000'
+		);
+	});
